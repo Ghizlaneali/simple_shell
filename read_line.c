16446dbb23356941c8091a9652678ecf5e5e9d45
Ghizlane Ali <ghizlaneali76@gmail.com>
2023-12-13 12:17:59 -0800

    shell

--- read_line.c
+++ read_line.c
@@ -1,28 +1,106 @@
 #include "shell.h"
 
 /**
- * read_line - reads line entered
+ * _getchar - a function that reads a character from the standard input
  *
- * RETURN: line
+ * Return: returns the read character
  */
 
-char *read_line(void)
+char _getchar(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t n;
-	
-	if (isatty(STDIN_FILENO))
-	{
-		write(STDOUT_FILENO, "$ ", 2);
-	}
-	n = getline(&line, &len, stdin);
+	char *buf;
+	char c;
+	int i = 0;
 
-	if ( n == -1)
+	buf = malloc(2);
+
+	if (!buf)
+		return (-1);
+
+	fflush(stdout);
+
+	i = read(1, buf, 1);
+	if (i == -1)
 	{
-		free(line);
-		return (NULL);
+	perror("Reading Char");
+		return (-1);
+	}
+	else if (i == 0)
+	{
+		write(1, "\n", 2);
+		fflush(stdout);
+	exit(1);
 	}
 
-	return (line);
+	c = *buf;
+
+	return (c);
+}
+
+/**
+ * _getline - a function to read a line from the standard input
+ * @line: a pointer to a pointer of location to save the string
+ * @len: the size of the characters read
+ *
+ * Return: returns the size of the read string
+ */
+
+int _getline(char **line, size_t *len)
+{
+	size_t limit = 25;
+	char *tmp;
+
+	line[0] = malloc(25);
+	if (!line[0])
+		return (-1);
+		*len = 0;
+
+	while (line[0][*len - 1] != '\n')
+	{
+		line[0][*len] = _getchar();
+		*len += 1;
+
+		if (*len > (limit - 3))
+		{
+			tmp = realloc(line[0], limit + 10);
+			if (tmp)
+				line[0] = tmp;
+			else
+				perror("Reallocation");
+				limit += 10;
+		}
+	}
+
+	return (*len);
+}
+
+/**
+ * echoer - a function to echo back any text you gave it
+ * @line: an array of command and arguments
+ *
+ * Return: returns 1 in success and -1 if it fails
+ */
+
+int echoer(char *line[])
+{
+	int i = 1;
+
+	if (line[i] == NULL)
+	{
+	write(1, "\n", 2);
+	return (1);
+	}
+
+	while (line[i] != NULL)
+	{
+		write(1, line[i], strleng(line[i]));
+		i++;
+		if (line[i] != NULL)
+		{
+			write(1, " ", 2);
+		}
+	}
+	write(1, "\n", 2);
+
+	return (1);
 }
--- string.c
+++ string.c
@@ -1,53 +1,167 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies the string pointed to by src into dest.
- * @dest: destination
- * @src: source
- * Return: char with copy of string
- */
+* getstr - a function to read a string from  the cli
+* @str: a string pointer
+*
+* Return: return 1 in success and -1 in failure
+*/
 
-char *_strcpy(char *dest, char *src)
+int getstr(char *str)
 {
-	int i;
-
-	for (i = 0; *(src + i) != '\0'; i++)
+	char **line;
+	size_t siz = 0;
+	signal(SIGINT, sigintHandler);
+	line = malloc(24);
+	if (!line)
 	{
-		dest[i] = *(src + i);
+		return (-1);
 	}
-	dest[i] = '\0';
 
-	return (dest);
+	if ((getline(line, &siz, stdin)) == -1)
+	{
+		return (-1);
+	}
+
+	line[0][siz] = '\0';
+
+	strcopy(line[0], str);
+
+	if (line[0][siz - 2] == '\\')
+	{
+		do {
+			str[strleng(str) - 1] = ' ';
+			siz = 0;
+			printprompt(1);
+
+			if ((getline(line, &siz, stdin)) == -1)
+			{
+				return (-1);
+			}
+			line[0][siz] = '\0';
+
+			strmix(line[0], str);
+		} while (line[0][siz - 2] == '\\');
+	}
+
+	free(line);
+	return (1);
 }
 
 /**
- * _strcmp - checks if 2 strings are of equal value
- * @s1: destination string
- * @s2: source string to be copied
- *
- * Return: difference of first characters that are of diff value
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
+* strcopy - a function to copy to strings
+* @src: the string to be copied
+* @dest: the destination of the copied string
+*
+* Return: returns nothing (void)
+*/
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+void strcopy(char *src, char *dest)
+{
+	int i = 0;
+	int j = 0;
+
+	if (!dest)
 	{
-		if (s1[i] != s2[i])
+		j = 0;
+	}
+
+	while (src[i] != '\0' && src[i] != '\n')
+	{
+		dest[j++] = src[i++];
+	}
+
+	dest[j] = '\0';
+}
+
+/**
+* strmix - a function to copy to strings and concatenate if
+*	   the destination have a string already.
+* @src: the string to be copied
+* @dest: the destination of the copied string
+*
+* Return: returns nothing (void)
+*/
+
+void strmix(char *src, char *dest)
+{
+	int i = 0;
+	int j;
+
+	if (!dest)
+	{
+		j = 0;
+	}
+	else
+	{
+		j = strleng(dest);
+	}
+
+	while (src[i] != '\0' && src[i] != '\n')
+	{
+		dest[j++] = src[i++];
+	}
+
+	dest[j] = '\0';
+}
+
+/**
+* strbrk - a function that breaks a long string in to arrauy of strings
+* @line: the string to be breakdown
+* @c: a chacter specifier where to break the string
+*
+* Return: returns the array of the words.
+*/
+
+char **strbrk(char *line, char c)
+{
+	int i = 0, j = 0;
+	char **array, **tmp;
+
+	if (!line || line[0] == '\0')
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(*array) * 128);
+	if (array == NULL)
+	{
+		perror("Couldn't Allocate");
+		return (NULL); }
+		while (line[i] != '\0')
+	{
+	if (j > 124)
+	{
+		tmp = realloc(array, sizeof(*array) * (i + 4));
+		if (tmp == NULL)
 		{
-			return (s1[i] - s2[i]);
+			perror("Couldn't Reallocate");
+			return (NULL); }
+		else
+		{
+			array = tmp; }
 		}
+		if (line[i] == c)
+		{
+			i++;
+			continue; }
+		array[j] = stringer(line, i, array[j], c);
+		if (array[j] == NULL)
+		{
+			return (NULL); }
+		i += strleng(array[j++]);
 	}
-	return (0);
+	array[j] = NULL;
+	return (array);
 }
 
 /**
- * _strlen - returns the length of a string.
- * @s: string
- * Return: length or string.
- */
+* strleng - measures a length of a string
+* @s: a string location pointer
+*
+* Return: returns length
+*/
 
-int _strlen(char *s)
+int strleng(char *s)
 {
 	int len = 0;
 
@@ -55,54 +169,6 @@ int _strlen(char *s)
 	{
 		len++;
 	}
+
 	return (len);
-}
-
-/**
-* _strncat - concatenates two strings,
-* @dest: destination.
-* @src: source.
-* @n: amount of bytes used from src.
-* Return: the pointer to dest.
-*/
-
-char *_strncat(char *dest, char *src, int n)
-{
-	int i = 0, j;
-
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; j < n && src[j] != '\0'; j++, i++)
-		dest[i] = src[j];
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * _strdup - Entry point
- *@str: string we need to duplicate
- * Return: a pointer to the duplicated string or NULL
- */
-
-char *_strdup(char *str)
-{
-	char *strnew = NULL;
-	unsigned int i;
-	int n;
-
-	if (str == NULL)
-		return (NULL);
-	for (n = 0; str[n] != '\0'; n++)
-		;
-	strnew = (char *)malloc(n + 1 * sizeof(char));
-	if (strnew != NULL)
-	{
-		for (i = 0; str[i] != '\0'; i++)
-			strnew[i] = str[i];
-	} else
-	{
-		return (NULL);
-	}
-	strnew[i] = '\0';
-	return (strnew);
 }
